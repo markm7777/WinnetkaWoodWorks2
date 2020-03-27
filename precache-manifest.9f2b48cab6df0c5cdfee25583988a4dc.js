c160b9535530426dbd22207846dca644083a3c13
markm7777 <mark.murphy7777@gmail.com>
2020-03-26 17:23:54 -0700

    Updates

---
+++ precache-manifest.9f2b48cab6df0c5cdfee25583988a4dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffdf0127ab2609445cf66b81c9ed34a",
+    "url": "/WinnetkaWoodWorks2/index.html"
+  },
+  {
+    "revision": "182e1e46daaba5a03b6f",
+    "url": "/WinnetkaWoodWorks2/static/css/main.4f2016da.chunk.css"
+  },
+  {
+    "revision": "7cf9a9ef95d732b5f995",
+    "url": "/WinnetkaWoodWorks2/static/js/2.9ee3a2a6.chunk.js"
+  },
+  {
+    "revision": "a47b34aba3d46fe2a07c18b19b3bc870",
+    "url": "/WinnetkaWoodWorks2/static/js/2.9ee3a2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182e1e46daaba5a03b6f",
+    "url": "/WinnetkaWoodWorks2/static/js/main.d348605f.chunk.js"
+  },
+  {
+    "revision": "9949c721a47ced4af66b",
+    "url": "/WinnetkaWoodWorks2/static/js/runtime-main.42c7b24c.js"
+  },
+  {
+    "revision": "4bcb1a94e05d502b3970c08b540a9e3a",
+    "url": "/WinnetkaWoodWorks2/static/media/Forest2.4bcb1a94.mp4"
+  }
+]);
